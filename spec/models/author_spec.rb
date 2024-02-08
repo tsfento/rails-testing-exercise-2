f61b7f5e59b43f84ce8d65119ea464a1fe3d80bf
Tyler Fenton <tsfento@gmail.com>
2024-02-08 14:05:11 -0600

    Finished tests for Exercise 2

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should return the correct books' do
+    author = Author.create(name: 'Martha Wells')
+    Book.create(title: 'All Systems Red', author_id: author.id)
+    Book.create(title: 'Artifical Condition', author_id: author.id)
+    Book.create(title: 'Rogue Protocol', author_id: author.id)
+    Book.create(title: 'Exit Strategy', author_id: author.id)
+    expect(author.books.to_json).to eq(Book.all.to_json)
+  end
+
+  it 'should also delete books when deleted' do
+    author = Author.create(name: 'Martha Wells')
+    Book.create(title: 'All Systems Red', author_id: author.id)
+    Book.create(title: 'Artifical Condition', author_id: author.id)
+    Book.create(title: 'Rogue Protocol', author_id: author.id)
+    Book.create(title: 'Exit Strategy', author_id: author.id)
+    author.destroy
+    expect(Book.all.to_a).to eq([])
+  end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should be valid if created with an author' do
+    author = Author.create(name: 'Martha Wells')
+    book = Book.create(title: 'All Systems Red', author_id: author.id)
+    expect(book).to be_valid
+  end
+
+  it 'should be invalid if not created with an author' do
+    book = Book.create(title: 'All Systems Red', author_id: nil)
+    expect(book).not_to be_valid
+  end
 end
